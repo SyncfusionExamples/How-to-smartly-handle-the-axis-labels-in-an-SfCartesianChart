049478d67f40cd5acc199073ee16859172c582f7
Sowndharya1911 <Sowndharya.Selladurai@syncfusion.com>
2024-06-14 18:08:43 +0530

    Sample updated

---
+++ SmartAxisLabelDemo/SmartAxisLabelDemo/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SmartAxisLabelDemo
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+}
